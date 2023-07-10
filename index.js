7b33bbcb4065ac6b09e67d95b9d79db576776dbd
Sadiq Omar Said <sadiq.said@student.moringaschool.com>
2023-07-10 21:55:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+// 1.Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+console.log(multiply); 
+
+// 2.Create a variable called random that will generate a random integer greater than 0
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+
+// 3.Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4
+var num3 = 28;
+var num4 = 6;
+var mod = num3 % num4;
+
+console.log(mod); 
+
+// 4.Create a variable called max that finds the highest number in a set; the value returned should be 20
+var set = [10, 5, 20, 15, 8];
+var max = Math.max(...set);
+
+console.log(max); 
+
+
